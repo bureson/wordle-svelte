6c54e59bd87eaf4eb7d967a5b0552ffc4858c759
Ondrej Bures <o.bures@gmail.com>
2023-06-30 14:54:52 +0200

    Build

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
-import firebase from 'svelte-adapter-firebase';
+import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,7 +6,13 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: firebase()
+		adapter: adapter({
+			out: 'public',
+            pages: 'public',
+            assets: 'public',
+            fallback: 'index.html',
+            precompress: false
+		})
 	}
 };
 
