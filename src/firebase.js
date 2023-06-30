76df90b8f02b0b66eae721548b99f85339894531
Ondrej Bures <o.bures@gmail.com>
2023-06-30 13:10:21 +0200

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDa-vNvKUj-Krj0btiUeNUuX_hMi_Z8M60",
+  authDomain: "wordle-svelte.firebaseapp.com",
+  projectId: "wordle-svelte",
+  storageBucket: "wordle-svelte.appspot.com",
+  messagingSenderId: "1087663216704",
+  appId: "1:1087663216704:web:2ada8d3da5fc19788227a5"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
